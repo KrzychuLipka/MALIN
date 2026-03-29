dca08ff21c6cbb51380a36a6039d15b63f216f18
Krzysztof Lipka <klipka1991@gmail.com>
2026-03-29 21:07:12 +0200

    Initial commit

---
+++ app/src/main/java/pl/lipov/malin/MainActivity.kt
@@ -0,0 +1,70 @@
+package pl.lipov.malin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import pl.lipov.malin.data.dataSources.BeaconDataSource
+import pl.lipov.malin.domain.model.Beacon
+import pl.lipov.malin.ui.MainViewModel
+import pl.lipov.malin.ui.theme.MALINTheme
+
+class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val BEACONS_FILE_NAME = "beacons.json"
+    }
+
+    private val dataSource = BeaconDataSource(
+        inputStreamProvider = { assets.open(BEACONS_FILE_NAME) }
+    )
+    private val viewModel = MainViewModel(dataSource)
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MALINTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BeaconList(
+                        viewModel = viewModel,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+
+        viewModel.loadBeacons()
+    }
+}
+
+@Composable
+fun BeaconList(
+    viewModel: MainViewModel,
+    modifier: Modifier
+) {
+    val beacons = viewModel.beacons.collectAsState()
+
+    LazyColumn(modifier) {
+        items(beacons.value) { beacon: Beacon ->
+            Text(
+                text = beacon.name,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
